1b08db544771ebf6b62bc2719fa67f8dcfc7fc3e
Ana Robakidze <ana.robakidze@celtra.com>
2018-10-12 16:52:31 -0400

    email

--- assets/mail.php
+++ assets/mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "tasnimakter777@gmail.com";
+        $recipient = "info@womenhelp.ge";
 
         // Set the email subject.
         $subject = "New contact from $name";
